e6184fa261e91e63ead77c920e90303675243217
Guillaume <guillaume@iMac-de-Fabrice.local>
2015-06-05 14:26:09 +0200

    Created edit form and preview of actual data

--- views/admin/edit.php
+++ views/admin/edit.php
@@ -8,7 +8,23 @@
 	<div class="small-12 medium-6 large-4 columns">
 		<!-- View item -->
 		<div class="admin-item-preview profile-item white-bg border">
-			
+			<?php foreach ($types as $key => $type) {
+					list($type, $params) = $type;
+					if ($type != 'hidden') { 
+						$method = "process_$type";
+					?>
+					
+					<div class="row">
+						<div class="small-12 medium-4 large-3 column">
+							<?=$titles[$key] ?>
+						</div>
+						<div class="small-12 medium-8 large-9 column">
+							<?=AdminType::$method($key, $values[$key], $params, 'display') ?>
+						</div>
+					</div>
+					
+				<?php }
+			} ?>
 		</div>
 	</div>
 	<div class="small-12 medium-6 large-8 columns">
@@ -16,6 +32,19 @@
 		<form method="post" action="" enctype="multipart/form-data" class="profile-item white-bg border">
 			<input type="hidden" name="MAX_FILE_SIZE" value="80000000" />
 			
+			<?php foreach ($types as $key => $type) {
+					list($type, $params) = $type;
+					$method = "process_$type";
+					?>
+					<div class="row">
+						<div class="small-12 medium-4 large-3 column">
+							<label class="inline"><?=$titles[$key] ?></label>
+						</div>
+						<div class="small-12 medium-8 large-9 column">
+							<?=AdminType::$method($key, $values[$key], $params, 'edit') ?>
+						</div>
+					</div>
+			<?php } ?>
 			
 			<div class="row">
 				<div class="small-12 column">
