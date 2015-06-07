459518b70a724f761804dd4038092e0e34111a03
Guillaume <contact@scrola.net>
2015-06-07 10:27:54 +0200

    Created ConfData trait.
    It enables the ::get, ::set, ::append, ::remove, and ::values for any object who needs data handling.
    Ex: Conf::get('tab.value');

--- core/Conf.php
+++ core/Conf.php
@@ -1,89 +1,15 @@
 <?php
 
 class 							Conf {
-	private static 				$__configuration = [];
-	
+    use ConfData;
+
 	public static function 		load($file) {
 		if (!file_exists($file))
 			return false;
 	
-		self::$__configuration = array_merge(self::$__configuration, json_decode(file_get_contents($file), true));
+		self::$__data = array_merge(self::$__data, json_decode(file_get_contents($file), true));
 		
 		return true;
-	}
-	
-	public static function 		get($var, $default = false) {
-		$els = explode('.', $var);
-		$current = self::$__configuration;
-		$l = sizeof($els) - 1;
-		foreach ($els as $i => $el) {
-			if ($i < $l) {
-				if (isset($current[$el]))
-					$current = $current[$el];
-				else
-					return $default;
-			} else {
-				if (isset($current[$el]))
-					return $current[$el];
-				return $default;
-			}
-		}
-	}
-
-    private static function 	recursiveSet($keys, $value, $data) {
-        $insertValue = sizeof($keys) == 1;
-        $key = array_shift($keys);
-
-        if ($insertValue) {
-            $data[$key] = $value;
-            return $data;
-        }
-        $data[$key] = self::recursiveSet($keys, $value, isset($data[$key]) ? $data[$key] : []);
-
-        return $data;
-    }
-
-    public static function 		set($k, $v) {
-        $k = explode('.', $k);
-        self::$__configuration = self::recursiveSet($k, $v, self::$__configuration);
-    }
-
-    private static function 	recursiveAppend($keys, $value, $data) {
-        $insertValue = sizeof($keys) == 1;
-        $key = array_shift($keys);
-
-        if ($insertValue) {
-            if (!isset($data[$key]))
-                $data[$key] = [];
-            $data[$key][] = $value;
-            return $data;
-        }
-        $data[$key] = self::recursiveAppend($keys, $value, isset($data[$key]) ? $data[$key] : []);
-
-        return $data;
-    }
-
-    public static function 		append($k, $v) {
-        $k = explode('.', $k);
-        self::$__configuration = self::recursiveAppend($k, $v, self::$__configuration);
-    }
-	
-	/*
-	** Use : Conf::values(function($k, $v){
-			echo "$k => $v <br />";
-		});
-	*/
-	public static function 		values($callback, $prefix = 'conf', $data = null) {
-		if (is_null($data))
-			$data = self::$__configuration;
-		
-		foreach ($data as $k => $v) {
-			if (is_array($v)) {
-				self::values($callback, $prefix.'.'.$k, $v);
-			}
-			else
-				$callback($prefix.'.'.$k, $v);
-		}
 	}
 }
 
--- core/Session.php
+++ core/Session.php
@@ -1,9 +1,10 @@
 <?php
 
 class 							Session {
+    use ConfData;
+
     public static               $apiMode = false;
     public static               $__expire = '+1 year';
-    private static 				$__data = [];
     public static               $id = false;
 
     public static function      init() {
@@ -92,78 +93,6 @@ class 							Session {
             self::$id = null;
         }
         self::save();
-    }
-
-    public static function 		get($var, $default = '') {
-        $els = explode('.', $var);
-        $current = self::$__data;
-        $l = sizeof($els) - 1;
-        foreach ($els as $i => $el) {
-            if ($i < $l) {
-                if (isset($current[$el]))
-                    $current = $current[$el];
-                else
-                    return $default;
-            } else {
-                if (isset($current[$el]))
-                    return $current[$el];
-                return $default;
-            }
-        }
-    }
-
-    private static function 	recursiveSet($keys, $value, $data) {
-        $insertValue = sizeof($keys) == 1;
-        $key = array_shift($keys);
-
-        if ($insertValue) {
-            $data[$key] = $value;
-            return $data;
-        }
-        $data[$key] = self::recursiveSet($keys, $value, isset($data[$key]) ? $data[$key] : []);
-
-        return $data;
-    }
-
-    public static function 		set($k, $v) {
-        $k = explode('.', $k);
-        self::$__data = self::recursiveSet($k, $v, self::$__data);
-    }
-
-    private static function 	recursiveRemove($keys, $data) {
-        $insertValue = sizeof($keys) == 1;
-        $key = array_shift($keys);
-
-        if ($insertValue) {
-            unset($data[$key]);
-            return $data;
-        }
-        $data[$key] = self::recursiveRemove($keys, $value, isset($data[$key]) ? $data[$key] : []);
-
-        return $data;
-    }
-
-    public static function 		remove($k) {
-        $k = explode('.', $k);
-        self::$__data = self::recursiveRemove($k, self::$__data);
-    }
-
-    /*
-    ** Use : Conf::values(function($k, $v){
-            echo "$k => $v <br />";
-        });
-    */
-    public static function 		values($callback, $prefix = 'session', $data = null) {
-        if (is_null($data))
-            $data = self::$__data;
-
-        foreach ($data as $k => $v) {
-            if (is_array($v)) {
-                self::values($callback, $prefix.'.'.$k, $v);
-            }
-            else
-                $callback($prefix.'.'.$k, $v);
-        }
     }
 }
 
