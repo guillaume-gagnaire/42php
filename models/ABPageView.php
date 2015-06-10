48dd1e6385b30d3baa43e31dde2d85d5f5655019
Guillaume <guillaume@iMac-de-Fabrice.local>
2015-06-10 13:32:57 +0200

    Created ABPageView model

---
+++ models/ABPageView.php
@@ -0,0 +1,13 @@
+<?php
+
+class 					ABPageView extends Model {
+	public 				$date;
+	public 				$path;
+	public 				$pagehash;
+	public 				$sessionid;
+	public 				$userid = 0;
+	public 				$clicked = 0;
+	public 				$click_date;
+}
+
+?>
