5c025c6b9872441daee74a532769169307a52ef9
Guillaume <guillaume@iMac-de-Fabrice.local>
2015-06-10 13:01:51 +0200

    AB class structure

---
+++ core/AB.php
@@ -0,0 +1,21 @@
+<?php
+
+class 							AB {
+	public static function 		render($opts) {
+		
+	}
+	
+	public static function 		partial($opts) {
+		
+	}
+	
+	public static function 		link($opts) {
+		
+	}
+	
+	public static function 		click($opts) {
+		
+	}
+}
+
+?>
