bc199a81553e9702cffdf2bc991bf8d9556024c6
Guillaume <contact@scrola.net>
2015-06-19 23:36:39 +0200

    SocialAuth: login and user info fetch

--- controllers/SocialAuthController.php
+++ controllers/SocialAuthController.php
@@ -6,16 +6,25 @@ class                           SocialAuthController extends Controller {
         if (isset($_GET['service']))
             $service = $_GET['service'];
 
-        $redirect = '/';
-        if (isset($_GET['redirect']))
-            $redirect = $_GET['redirect'];
+        if (isset($_GET['redirect'])) {
+            Session::set('SocialAuth.Redirect', $_GET['redirect']);
+        }
 
         if (!$service)
-            Redirect::http(Argv::createUrl('login').'?redirect='.urlencode($redirect));
+            Redirect::http(Argv::createUrl('login').(isset($_GET['redirect']) ? '?redirect='.urlencode($_GET['redirect']) : ''));
 
         $auth = new SocialAuth($service);
 
         $user = $auth->auth();
+
+        var_dump($user);
+
+
+
+        $redirect = Session::get('SocialAuth.Redirect', '/');
+        Session::remove('SocialAuth');
+        Session::save();
+        //Redirect::http($redirect);
     }
 }
 
--- core/SocialAuth.php
+++ core/SocialAuth.php
@@ -1,5 +1,23 @@
 <?php
 
+session_start();
+
+/**
+ * Facebook
+ */
+define('FACEBOOK_SDK_V4_SRC_DIR', ROOT.'/lib/facebook/src/Facebook/');
+require ROOT.'/lib/facebook/autoload.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookRedirectLoginHelper;
+
+/**
+ * Google
+ */
+require_once ROOT.'/lib/google/src/Google/autoload.php';
+
 class                       SocialAuth {
     private                 $provider;
 
@@ -22,11 +40,87 @@ class                       SocialAuth {
      *  provider_id
      *  firstname
      *  lastname
-     *  genre
+     *  genre (male || female)
      *  email_verified
+     *  photo
      * }
      */
     private function         facebook() {
+        FacebookSession::setDefaultApplication(
+            Conf::get('auth.social.facebook.app_id', ''),
+            Conf::get('auth.social.facebook.app_secret', '')
+        );
+        $helper = new FacebookRedirectLoginHelper('http://'.$_SERVER['HTTP_HOST'].Argv::createUrl('socialauth').'?service=facebook&receive');
+
+        try {
+            $session = $helper->getSessionFromRedirect();
+        } catch (FacebookRequestException $e) {
+            echo $e->getMessage();
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+
+        if (isset($_GET['receive'])) {
+            $request = new FacebookRequest($session, 'GET', '/me');
+            $response = $request->execute();
+            $graphObject = $response->getGraphObject();
+
+            return [
+                'email' => $graphObject->getProperty('email'),
+                'provider' => 'facebook',
+                'provider_id' => $graphObject->getProperty('id'),
+                'firstname' => $graphObject->getProperty('first_name'),
+                'lastname' => $graphObject->getProperty('last_name'),
+                'genre' => $graphObject->getProperty('gender'),
+                'email_verified' => $graphObject->getProperty('verified'),
+                'photo' => 'http://graph.facebook.com/'.$graphObject->getProperty('id').'/picture?type=large'
+            ];
+        } else {
+            $loginUrl = $helper->getLoginUrl([
+                'scope' => 'email'
+            ]);
+            Redirect::http($loginUrl);
+        }
+    }
+
+    private function        google() {
+        $client_id = Conf::get('auth.social.google.client_id', '');
+        $client_secret = Conf::get('auth.social.google.client_secret', '');
+        $redirect_uri = 'http://'.$_SERVER['HTTP_HOST'].Argv::createUrl('socialauth').'?service=google&receive';
+
+        $client = new Google_Client();
+        $client->setClientId($client_id);
+        $client->setClientSecret($client_secret);
+        $client->setRedirectUri($redirect_uri);
+        $client->addScope('https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/plus.login');
+
+        if (isset($_GET['code'])) {
+            $client->authenticate($_GET['code']);
+            $_SESSION['access_token'] = $client->getAccessToken();
+            $redirect = 'http://'.$_SERVER['HTTP_HOST'].Argv::createUrl('socialauth').'?service=google&receive';
+            header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
+        }
+
+        if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+            $client->setAccessToken($_SESSION['access_token']);
+        } else {
+            Redirect::http($client->createAuthUrl());
+        }
+
+        if ($client->getAccessToken()) {
+            $plus = new Google_Service_Oauth2($client);
+            $me = $plus->userinfo->get();
+            return [
+                'email' => $me->email,
+                'provider' => 'google',
+                'provider_id' => $me->id,
+                'firstname' => $me->givenName,
+                'lastname' => $me->familyName,
+                'genre' => $me->gender,
+                'email_verified' => $me->verifiedEmail,
+                'photo' => $me->picture
+            ];
+        }
 
     }
 }
