016c52a4d7915563c4dd2800c5e50bf24e2d1f55
Guillaume <contact@scrola.net>
2015-06-19 21:34:52 +0200

    Deleted HybridAuth Framework -> going to custom classes

--- controllers/SocialAuthController.php
+++ controllers/SocialAuthController.php
@@ -13,22 +13,6 @@ class                           SocialAuthController extends Controller {
         if (!$service)
             Redirect::http(Argv::createUrl('login').'?redirect='.urlencode($redirect));
 
-        $config_file_path = ROOT.'/lib/hybridauth/config.php';
-
-        // Load lib
-        require_once ROOT.'/lib/hybridauth/Hybrid/Auth.php';
-        // Starting
-
-        try {
-            $hybridauth = new Hybrid_Auth($config_file_path);
-
-            $provider = $hybridauth->authenticate($service);
-
-            var_dump($provider);
-
-        } catch (Exception $e) {
-
-        }
     }
 }
 
