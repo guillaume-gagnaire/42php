759e9103fc473b1d28eae1400056b8b0077935f1
Guillaume Gagnaire <alduro.estallas@gmail.com>
2016-01-13 11:32:39 +0000

    Added keyval & dz support

--- views/admin/edit.php
+++ views/admin/edit.php
@@ -1,3 +1,62 @@
+<script type="text/javascript" src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/lib/dropzone/dropzone.css" />
+<script type="text/javascript" src="/lib/dropzone/dropzone.js"></script>
+<link rel="stylesheet" type="text/css" href="/lib/tags/min.css" />
+<script type="text/javascript" src="/lib/tags/min.js"></script>
+<script type="text/javascript">
+	Dropzone.autoDiscover = false;
+	function addLineToKeyval(key) {
+		$('#keyval_'+key+'_items').append('<div class="row">'+
+			'<div class="small-5 column">'+
+				'<input type="text" name="" value="" propname="key" placeholder="Nom" />'+
+			'</div>'+
+			'<div class="small-5 column">'+
+				'<input type="text" name="" value="" propname="value" placeholder="Valeur" />'+
+			'</div>'+
+			'<div class="small-2 column">'+
+				'<a href="#" onclick="var el = this.parentNode.parentNode; if (el) {el.parentNode.removeChild(el);} reloadKeyvalIds(\''+key+'\'); return false;"><i class="fi-x" style="font-size: 16px; color: red; margin-top: 10px; display: inline-block;"></i></a>'+
+			'</div>'+
+		'</div>');
+		reloadKeyvalIds(key);
+	}
+	function reloadKeyvalIds(key) {
+		var p = document.getElementById('keyval_'+key+'_items');
+		if (p) {
+			var els = p.getElementsByClassName('row');
+			for (var i = 0; i < els.length; i++) {
+				$(els[i]).find('[propname=key]').attr('name', key+'['+i+'][key]');
+				$(els[i]).find('[propname=value]').attr('name', key+'['+i+'][value]');
+			}
+		}
+	}
+	function updateDzField(dz) {
+		if (!dz) 
+			return;
+		for (var i = 0; i < dz.files.length; i++) {
+			dz.files[i].previewElement.setAttribute('data-order', i);
+			dz.files[i].order = i;
+		}
+		var input = document.getElementById(dz.element.getAttribute('data-result'));
+		var f = [];
+		for (var i = 0; i < dz.files.length; i++) {
+			if (dz.files[i].finalPath)
+				f.push(dz.files[i].finalPath);
+		}
+		input.value = f.join(';');
+	}
+	function sortDz(dz) {
+		var newFiles = [];
+		$(dz.element).find('.dz-preview').each(function(){
+			var order = parseInt($(this).attr('data-order'));
+			for (var i = 0; i < dz.files.length; i++) {
+				if (dz.files[i].order == order)
+					newFiles.push(dz.files[i]);
+			}
+		});
+		dz.files = newFiles;
+		updateDzField(dz);
+	}
+</script>
 <ul class="breadcrumbs admin-ariane">
     <li><a href="<?=Argv::createUrl('admin') ?>"><?= _t("Panneau d'administration") ?></a></li>
     <li><a href="<?=Conf::get('admin.url') ?>"><?=Conf::get('admin.moduleTitle') ?></a></li>
