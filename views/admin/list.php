1394f9a6a093f57d5881c72f2a8efc6b48c5c5a2
Guillaume Gagnaire <alduro.estallas@gmail.com>
2016-02-12 14:14:38 +0000

    Added sortable and filter handling

--- views/admin/list.php
+++ views/admin/list.php
@@ -8,4 +8,40 @@
 
 <?=$items ?>
 
-<?=$pagination ?>
+<?php if ($sortable) { ?>
+	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	<script type="text/javascript">
+		$(function(){
+			$(".auto-list").sortable({
+				items: 'tbody tr',
+				update: function(event, ui){
+					var data = [];
+					$('.auto-list tbody tr').each(function(){
+						data.push($(this).attr('data-id'));
+					});
+					$.ajax({
+						url: '<?= Argv::createUrl('admin') ?>?module=updateTableOrder&field=<?= urlencode($sortable) ?>&tableName=<?= urlencode($sortable_table) ?>&list=' + encodeURIComponent(data.join(','))
+					});
+				}
+			});
+		});
+	</script>
+<?php } ?>
+
+<?= !$filter ? $pagination : '' ?>
+
+<?php if ($filter) { ?>
+	<style type="text/css">
+		.dataTables_wrapper select, .dataTables_wrapper input {
+			display: inline-block;
+			width: auto;
+		}
+	</style>
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/s/dt/jq-2.1.4,dt-1.10.10,fh-3.1.0/datatables.min.css"/>
+	<script type="text/javascript" src="https://cdn.datatables.net/s/dt/jq-2.1.4,dt-1.10.10,fh-3.1.0/datatables.min.js"></script>
+	<script type="text/javascript">
+		$(function(){
+			$('.auto-list').DataTable();
+		});
+	</script>
+<?php } ?>
